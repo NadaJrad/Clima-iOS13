bc58326ba005f544879a5cccd5e9a929e0610337
Nada <nadajradbenaicha@gmail.com>
2025-07-23 11:03:21 +0200

    checking error for the handle methode

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -14,18 +14,21 @@ struct WeatherManager {
         // 1- create a URL
         
         if let url = URL(string: urlString){
-        // 2- create a urlSession
+            // 2- create a urlSession
             
             let session = URLSession(configuration: .default)
-        // 3- give the session a task
+            // 3- give the session a task
             
             let task = session.dataTask(with: url, completionHandler:handle(data:response:error:))
-        // 4- start task
+            // 4- start task
             task.resume()
         }
-        
+    }
         func handle (data:Data?, response:URLResponse?, error:Error?){
-            
+            if error != nil {
+                print(error!)
+                return
+            }
         }
     }
-}
+
