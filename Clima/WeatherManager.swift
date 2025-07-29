3dc0123f616107e674b03e0570253153c6c49fb1
Nada <nadajradbenaicha@gmail.com>
2025-07-29 10:24:31 +0200

    addind with a swift statment the condition name of the weather

--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -37,11 +37,21 @@ struct WeatherManager {
         let decoder = JSONDecoder()
         do {
           let decodeData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodeData.weather[0].id)
+            let id = decodeData.weather[0].id
+            let temp = decodeData.main.temp
+            let name = decodeData.name
+            
+            let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
+            print(weather.getConditionName(weatherId: id))
         } catch {
             print(error)
         }
         
     }
+    
+    
+    
+
     }
+    
 
